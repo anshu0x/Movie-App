e09fa0e976eb85e2ca742972bcb4a0dc5aaa372b
anshu0x <anshusharma6327@gmail.com>
2022-09-14 23:26:57 +0530

    search bar removed from single movie page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Badge, Nav } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { useContext } from "react";
@@ -9,7 +9,8 @@ import { useState } from "react";
 function NavScrollExample() {
   const setMovieTitle = useContext(MovieContext);
   const [movie, setmovie] = useState("");
-
+  let location = useLocation();
+  console.log(location.pathname.includes("movie"));
   return (
     <Navbar bg="dark" expand="lg" variant="dark">
       <Container fluid>
@@ -28,7 +29,7 @@ function NavScrollExample() {
                   <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0v6h8V1H4zm8 8H4v6h8V9zM1 1v2h2V1H1zm2 3H1v2h2V4zM1 7v2h2V7H1zm2 3H1v2h2v-2zm-2 3v2h2v-2H1zM15 1h-2v2h2V1zm-2 3v2h2V4h-2zm2 3h-2v2h2V7zm-2 3v2h2v-2h-2zm2 3h-2v2h2v-2z" />
                 </svg>
               </span>
-             <span> Movie Info App</span>
+              <span> Movie Info</span>
             </Badge>
           </Link>
         </Navbar.Brand>
@@ -39,32 +40,34 @@ function NavScrollExample() {
             style={{ maxHeight: "100px" }}
             navbarScroll
           ></Nav>
-          <Form
-            onSubmit={(e) => e.preventDefault()}
-            className="d-flex justify-content-center align-items-center"
-          >
-            <input
-              className="me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-              style={{ outline: "none", border: "none" }}
-              value={movie}
-              onChange={(e) => setmovie(e.target.value)}
-            />
-            <button
-              style={{
-                border: "#fff 1px solid",
-                background: "black",
-                color: "white",
-                cursor: "pointer",
-              }}
-              disabled={movie.length < 3}
-              onClick={() => setMovieTitle.setMovieTitle(movie)}
+          {!location.pathname.includes("movie") && (
+            <Form
+              onSubmit={(e) => e.preventDefault()}
+              className="d-flex justify-content-center align-items-center"
             >
-              Search
-            </button>
-          </Form>
+              <input
+                className="me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                style={{ outline: "none", border: "none" }}
+                value={movie}
+                onChange={(e) => setmovie(e.target.value)}
+              />
+              <button
+                style={{
+                  border: "#fff 1px solid",
+                  background: "black",
+                  color: "white",
+                  cursor: "pointer",
+                }}
+                disabled={movie.length < 3}
+                onClick={() => setMovieTitle.setMovieTitle(movie)}
+              >
+                Search
+              </button>
+            </Form>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
