607c6c3633c2ec09b6ffb917b4420cbeac15fc63
anshu0x <anshusharma6327@gmail.com>
2022-09-25 22:30:51 +0530

    _redirect file added fulls screen api inegrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React,{useEffect} from "react";
 import Navbar from "./components/NavBar";
 import { Routes, Route, Link } from "react-router-dom";
 import Movie from "./components/Movie";
 import SingleMovie from "./components/SingleMovie";
 const App = () => {
+  function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      toggleFullScreen();
+    }, 2000);
+
+    document.addEventListener(
+      "keydown",
+      (e) => {
+        if (e.key === "Enter") {
+          toggleFullScreen();
+        }
+      },
+      false
+    );
+  }, []);
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Movie />} />
-        <Route
-          path="/movie/:id"
-          element={
-            <SingleMovie/>
-          }
-        />
+        <Route path="/movie/:id" element={<SingleMovie />} />
       </Routes>
     </>
   );
